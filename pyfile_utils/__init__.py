3e16f342fd6e7518508ee7b97e5069bbb60e9f0d
nw13slx <nw13mifaso@gmail.com>
2022-02-16 15:30:49 -0800

    update init

--- pyfile_utils/__init__.py
+++ pyfile_utils/__init__.py
@@ -9,6 +9,7 @@ from .auto_init import (
 from .savenload import (
     save_file,
     load_file,
+    load_callable,
     atomic_write,
     finish_all_writes,
     atomic_write_group,
@@ -22,6 +23,7 @@ __all__ = [
     get_w_prefix,
     save_file,
     load_file,
+    load_callable,
     atomic_write,
     finish_all_writes,
     atomic_write_group,
